0fc8c1892bc9ef19eed01b318305b0ace5710248
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-04-03 16:37:55 +0500

    JournalItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import JournalItem from "./components/JournalItem/JournalItem";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>Заголовок</h1>
       <p>Заголовок</p>
       <Button />
+      <JournalItem />
     </>
   );
 }
